a21cdc751cf8a297470c58ef253670678bdcee9d
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-04 18:23:12 -0400

    fix issue where indicing was broken and made quick and dirty tester program

--- hw-07.c
+++ hw-07.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 
-int printIntArr(int array[]){
-    int arrayLength = sizeof array / sizeof array[0];
+int printIntArr(int array[], int length){
     int i;
     printf("[ ");
-    for (i = 0; i<arrayLength; i++){
+    for (i = 0; i<length; i++){
         printf("%d ", array[i]);
     }
     printf("]\n");
     return 0;
 }
 
-int calcAvg(int array[]){
-    int arrayLength = sizeof array / sizeof array[0];
+double calcAvg(int array[], int length){
     int total = 0;
     int i;
-    for (i = 0; i < arrayLength; i++)
+    for (i = 0; i < length; i++)
     {
         total += array[i];
     }
-    return total / arrayLength;
+    return total * 1.0 / length;
 }
 
-int cloneArray(int copyFrom[], int copyTo[]){
-    if (sizeof copyFrom / sizeof copyFrom [0] == sizeof copyTo / sizeof copyTo[0]){
-        int arrayLength = sizeof copyFrom / sizeof copyFrom[0];
-        int i;
-        for (i = 0; i < arrayLength; i++){
-            copyTo[i] = copyFrom[i];
-        }
-        return 0;
+int cloneArray(int copyFrom[], int copyTo[], int length){
+    int i;
+    for (i = 0; i < length; i++){
+        copyTo[i] = copyFrom[i];
     }
-    else
-    {
-        return 1;
-    }
+    return 0;
+}
+
+int main(){
+    int array[5] = {1, 2, 3, 4, 5};
+    printIntArr(array, sizeof(array) / sizeof(int));
+    printf("%0.5f\n", calcAvg(array, sizeof(array) / sizeof(int)));
+    int anotherArray[5];
+    cloneArray(array, anotherArray, sizeof(array) / sizeof(int));
+    printIntArr(array, sizeof(array) / sizeof(int));
+    printIntArr(anotherArray, sizeof(anotherArray) / sizeof(int));
 }

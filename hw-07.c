7b4e6733abef29aa7813e1dd48fe6f3c2adaaa00
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-04 15:18:22 -0400

    part 1 hw-07

---
+++ hw-07.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int printIntArr(int[] array){
+    int arrayLength = sizeof array / sizeof array[0];
+    int i;
+    printf("[ ");
+    for (i = 0; i<arrayLength; i++){
+        printf("%d ", array[i]);
+    }
+    printf("]\n");
+}
